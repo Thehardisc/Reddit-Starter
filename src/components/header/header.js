fd256197cf1b75b7a40563226664507066dcc526
Thehardisc <82160579+Thehardisc@users.noreply.github.com>
2021-09-01 18:57:53 +0300

    cleanning up from the first libery

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './header.css'
+
+
+export const Header = () => {
+    return (
+        <div className="Header">
+            <ul>
+                <li><a href="www.google.com">Hello</a></li>
+            </ul>
+        </div>
+    )
+}
